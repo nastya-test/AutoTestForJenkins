0a39b3ec73b250f0dba985b5e3e8f556e69313cc
nastyatest <nastyatest>
2022-10-18 10:16:55 +0300

    не помню что

--- src/test/java/cucumber/steps/Hooks.java
+++ src/test/java/cucumber/steps/Hooks.java
@@ -1,7 +1,17 @@
 package cucumber.steps;
 
+import com.codeborne.selenide.Selenide;
+import cucumber.pageobjects.mainPage.FilmTicketsPage;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import test.BaseTest;
+
+import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.Selenide.sleep;
+import static cucumber.map.PageMap.savePageMap;
+import static cucumber.map.PageMap.setPopularPages;
+import static cucumber.steps.AbstractSteps.bestScroll;
 
 public class Hooks {
     @After("@success")
@@ -11,6 +21,14 @@ public class Hooks {
         System.out.println(scenario.getStatus());
         System.out.println(scenario.isFailed());
         System.out.println(scenario.getSourceTagNames());
+    }
+
+    @Before("@filmTickets")
+    public void setFilmTickets() {
+        BaseTest.baseOpenPage();
+        setPopularPages(savePageMap.get("Блок Билеты в кино"));
+        bestScroll(page(FilmTicketsPage.class).filmTicketsBlock);
+        sleep(2000);
     }
 
 }
