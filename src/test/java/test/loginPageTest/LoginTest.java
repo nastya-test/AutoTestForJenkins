fe258751606bc945d88ade5c17095009f3a20f7f
nastyatest <nastyatest>
2022-10-20 13:52:52 +0300

    jenkins try

--- src/test/java/test/loginPageTest/LoginTest.java
+++ src/test/java/test/loginPageTest/LoginTest.java
@@ -3,11 +3,10 @@ package test.loginPageTest;
 import page.authorizationPage.LoginPage;
 import io.qameta.allure.Description;
 import test.BaseTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import static com.codeborne.selenide.Selenide.page;
 
-public class LoginTest {
+public class LoginTest extends BaseTest{
 
     @Test
     @Description("Успешный вход в систему")
